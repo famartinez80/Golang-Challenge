6cf681248eb3f1617be8034a9fac2d7fb5dc34c4
Fredy Martinez <ext-f.martinez@rappi.com>
2021-05-18 20:21:59 -0500

    Implement parallelize for getPriceFor

--- cache.go
+++ cache.go
@@ -2,6 +2,7 @@ package sample1
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -17,43 +18,49 @@ type PriceService interface {
 type TransparentCache struct {
 	actualPriceService PriceService
 	maxAge             time.Duration
-	prices             map[string]float64
+	prices             *sync.Map
 }
 
 func NewTransparentCache(actualPriceService PriceService, maxAge time.Duration) *TransparentCache {
 	return &TransparentCache{
 		actualPriceService: actualPriceService,
 		maxAge:             maxAge,
-		prices:             map[string]float64{},
+		prices:             &sync.Map{},
 	}
 }
 
 // GetPriceFor gets the price for the item, either from the cache or the actual service if it was not cached or too old
 func (c *TransparentCache) GetPriceFor(itemCode string) (float64, error) {
-	price, ok := c.prices[itemCode]
+	priceI, ok := c.prices.Load(itemCode)
 	if ok {
 		// TODO: check that the price was retrieved less than "maxAge" ago!
-		return price, nil
+		return priceI.(float64), nil
 	}
 	price, err := c.actualPriceService.GetPriceFor(itemCode)
 	if err != nil {
 		return 0, fmt.Errorf("getting price from service : %v", err.Error())
 	}
-	c.prices[itemCode] = price
+	c.prices.Store(itemCode, price)
 	return price, nil
 }
 
 // GetPricesFor gets the prices for several items at once, some might be found in the cache, others might not
 // If any of the operations returns an error, it should return an error as well
 func (c *TransparentCache) GetPricesFor(itemCodes ...string) ([]float64, error) {
-	results := []float64{}
+	var results []float64
+	var wg sync.WaitGroup
+
 	for _, itemCode := range itemCodes {
-		// TODO: parallelize this, it can be optimized to not make the calls to the external service sequentially
-		price, err := c.GetPriceFor(itemCode)
-		if err != nil {
-			return []float64{}, err
-		}
-		results = append(results, price)
+		wg.Add(1)
+		go func(itemCode string) {
+			defer wg.Done()
+			price, err := c.GetPriceFor(itemCode)
+			if err != nil {
+				fmt.Println(err)
+			}
+			results = append(results, price)
+		}(itemCode)
 	}
+	wg.Wait()
 	return results, nil
 }
